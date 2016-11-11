4d6d5c786b923decf5c2f856d80df4fd04dbe3d4
Rick van 't Wout Hofland <rick@nbccongrescentrum.nl>
2016-11-11 14:52:39 +0100

    added ID to route|

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-client',
   templateUrl: 'client.component.html'
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
+  public sub: any;
+  public id: string;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) {}
+
+  ngOnInit(){
+    this.sub = this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+  }
 
 }
