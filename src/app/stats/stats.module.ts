2a2ba7f5cc4497cf59fd5bb6977e23d6f233dfbc
Jens Hoevenaars <jens@nbccongrescentrum.nl>
2016-11-11 15:34:11 +0100

    Added shit

--- src/app/stats/stats.module.ts
+++ src/app/stats/stats.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { StatsComponent } from './stats.component';
 import { CommonModule } from '@angular/common';
 import { routing } from './stats.routing';
+import { ChartModule } from 'angular2-highcharts';
 
 @NgModule({
   imports: [
     CommonModule,
+    ChartModule,
     routing
   ],
   declarations: [
