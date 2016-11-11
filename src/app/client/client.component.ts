17de5b6130752462d5f08d89ec9917e90b381c94
Rick van 't Wout Hofland <rick@nbccongrescentrum.nl>
2016-11-11 17:30:48 +0100

    updated client

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit }        from '@angular/core';
 import { Router, ActivatedRoute }   from '@angular/router';
-import { Http, Headers }            from "@angular/http";
+import { Http, Headers, Response }  from "@angular/http";
 import { ApiService }               from '../services/api.service';
-
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-client',
@@ -11,8 +11,10 @@ import { ApiService }               from '../services/api.service';
 export class ClientComponent implements OnInit {
   public sub: any;
   public id: string;
+  public question: string;
   public headers: any = new Headers({ 'Content-Type': 'application/json' });
   public root: string = 'http:/happy-stat.herokuapp.com/';
+  public errorMessage: any;
 
   constructor(
     private router: Router,
@@ -21,17 +23,40 @@ export class ClientComponent implements OnInit {
     private http: Http ) {}
 
   ngOnInit(){
+    var reply: any;
+
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id'];
     });
+
+    this.getLocation(this.id).then(
+      location => this.question = location.question,
+      error => this.errorMessage = <any>error);
+  }
+
+  getLocation(location: string) {
+    return this.http.get('https://happy-stat.herokuapp.com/api/locations/'+location)
+      .toPromise()
+      .then(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body;
+  }
+
+  private handleError(error: Response | any) {
+    console.log(error);
+    return Promise.reject(error);
   }
 
   add(amount: number){
 
-    var userinfo: any;
+    var reply: any;
 
     this.apiService.add(this.id, amount).subscribe(
-      data => { userinfo = data },
+      data => { reply = data },
       err => console.log('error'),
       () => {
         console.log('done');
